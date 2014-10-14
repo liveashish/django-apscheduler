4032defda2dcd6af5237c3fede10ef1cfa8c8f8a
Ashish Kumar Sahoo <ashish.sahoo@gmail.com>
2014-10-14 21:42:41 +0530

    fixed indention

--- app/management/commands/command.py
+++ app/management/commands/command.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand, CommandError
 from datetime import datetime
 import time
 import os
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.background import BackgroundScheduler 
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
@@ -16,16 +16,16 @@ class Command(BaseCommand):
          print "This is a command"
          print('Tick! The time is: %s' % datetime.now())
 
-	
-		 if __name__ == '__main__':
-		     scheduler = BackgroundScheduler()
-		     scheduler.add_job(handle, 'interval', seconds=3)
-		     scheduler.start()
-		     #print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
-		 
-		     try:
-		        # This is here to simulate application activity (which keeps the main thread alive).
-		         while True:
-		             time.sleep(10)
-		     except (KeyboardInterrupt, SystemExit):
-		         scheduler.shutdown()  # Not strictly necessary if daemonic mode is enabled but should be done if possible
+          
+         #if __name__ == '__main__':
+         scheduler = BackgroundScheduler()
+         scheduler.add_job(handle, 'interval', seconds=3)
+         scheduler.start()
+         print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+         
+         try:
+                # This is here to simulate application activity (which keeps the main thread alive).
+             while True:
+                 time.sleep(10)
+         except (KeyboardInterrupt, SystemExit):
+             scheduler.shutdown()  # Not strictly necessary if daemonic mode is enabled but should be done if possible
